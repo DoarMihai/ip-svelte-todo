36c3d246f34100cd4cd9f705cfbfddc8cdafbb75
Stefanescu Mihai <mihai@invata-programare.ro>
2022-12-29 20:31:10 +0200

    progress on the svelte todo app

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,105 @@
 <script>
-	export let name;
+	import "./css/reset.css";
+	import "./css/style.css";
+
+	let todoTitle = '';
+	let todoId = 4;
+
+	let todos = [
+		{
+			id: 1,
+			title: 'Termina articolul despre sveltejs',
+			isComplete: false,
+			isEditing: false,
+		},
+		{
+			id: 2,
+			title: 'Cumpara bacon',
+			isComplete: true,
+			isEditing: false,
+		},
+		{
+			id: 3,
+			title: 'Cucereste lumea',
+			isComplete: false,
+			isEditing: false,
+		},
+	];
+
+	function addTodo(event) {
+		event.preventDefault();
+
+		todos = [
+			...todos,
+			{
+				id: todoId,
+				title: todoTitle,
+				isComplete: false,
+				isEditing: false,
+			}
+		];
+
+		todoTitle = '';
+		todoId++;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="todo-app-container">
+  <div class="todo-app">
+    <h2>Todo App</h2>
+    <form action="#" on:submit={addTodo}>
+      <input
+        type="text"
+        class="todo-input"
+        placeholder="What do you need to do?"
+		bind:value={todoTitle}
+      />
+    </form>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    <ul class="todo-list">
+		{#each todos as todo}
+			<li class="todo-item-container">
+				<div class="todo-item">
+				<input type="checkbox" />
+				<span class="todo-item-label">{todo.title}</span>
+				<!-- <input type="text" class="todo-item-input" value="Finish Svelte Series"/> -->
+				</div>
+				<button class="x-button">
+				<svg
+					class="x-button-icon"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth={2}
+					d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+				</button>
+			</li>
+		{/each}
+    </ul>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    <div class="check-all-container">
+      <div>
+        <div class="button">Check All</div>
+      </div>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+      <span>3 items remaining</span>
+    </div>
+
+    <div class="other-buttons-container">
+      <div>
+        <button class="button filter-button filter-button-active"> All </button>
+        <button class="button filter-button">Active</button>
+        <button class="button filter-button">Completed</button>
+      </div>
+      <div>
+        <button class="button">Clear completed</button>
+      </div>
+    </div>
+  </div>
+</div>
